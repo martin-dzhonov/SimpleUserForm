9bea18385af06b47bb0dab39b005097b15f952b2
Martin Dzhonov <martin.dzhonov@gmail.com>
2014-12-18 12:50:35 -0800

    sync

--- SimpleUserForm/LoginViewController.m
+++ SimpleUserForm/LoginViewController.m
@@ -82,11 +82,9 @@
     [button setTitleColor:[UIColor grayColor] forState:UIControlStateDisabled];
     [button setBackgroundColor:[UIColor colorWithRed:0 green:255 blue:0 alpha:0.4]];
     CALayer *layer = button.layer;
-    //UIColor *myColor = [UIColor colorWithRed:8.0 green:8.0 blue:8.0 alpha:0.4];
-    //layer.backgroundColor = [myColor CGColor];
     layer.borderColor = [[UIColor blackColor] CGColor];
     layer.cornerRadius = 2.0f;
-    layer.borderWidth = 1.0f;
+    layer.borderWidth = 1.2f;
 
 }
 
