d219d52b5f87e6c18a43d0a02ff79dfab5e1b131
Martin Dzhonov <martin.dzhonov@gmail.com>
2014-12-18 01:52:58 -0800

    Initial

---
+++ SimpleUserForm/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  SimpleUserForm
+//
+//  Created by Gosho Goshev on 12/17/14.
+//  Copyright (c) 2014 Gosho Goshev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end
